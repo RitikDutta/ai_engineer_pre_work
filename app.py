4a5b3297794a6154f37565305b0f3641234b3456
ritik dutta <ritikduttagd@gmail.com>
2025-04-17 18:39:23 +0530

    added calling llm

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+from my_assistant import MyAssistant
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    Assistant = MyAssistant()
+    transcript = ''
+    summary = ''
     if request.method == 'POST':
         transcript = request.form['transcript']
         print(transcript)
-    return render_template('index.html', summary = transcript)
+        summary = Assistant.generate(transcript)
+    return render_template('index.html', summary=summary)
 
 
 if __name__ == '__main__':
